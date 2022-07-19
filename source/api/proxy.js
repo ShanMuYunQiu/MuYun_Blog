ce5c9ab06f408f501b8698cc4e117c9fc4a80971
圣奇宝枣 <lty15376767691@gmail.com>
2022-07-19 16:47:01 +0800

    完成跨域请求

--- source/api/proxy.js
+++ source/api/proxy.js
@@ -7,7 +7,7 @@ module.exports = (req, res) => {
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/backend')) {
-    target = 'https://v1.hitokoto.cn?c=a&c=d&c=e&c=f&c=g&c=i&c=j&c=k', "https://waline-swart-ten.vercel.app"
+    target = 'https://v1.hitokoto.cn?c=a&c=d&c=e&c=f&c=g&c=i&c=j&c=k'
   }
 
   // 创建代理对象并转发请求
